e158ac9c62cb34ef5ce95a3c993591ba7d6e6a58
Daniel <skar.dpm@gmail.com>
2023-01-09 11:22:31 -0500

    feat: simulate data stores

---
+++ src/app/data/stores.ts
@@ -0,0 +1,146 @@
+import { Store } from "../utils/types"
+
+export const dataStores: Store[] = [
+    {
+        id: 1,
+        name: 'ZAPATERIA VAINILLITA',
+        state: 'AGUASCALIENTES',
+        city: 'AGUASCALIENTES',
+        neighborhood: 'PILAR BLANCO INFONAVIT',
+        address: 'JOSE MARIA CHAVEZ',
+        numInt: 'B4',
+        cp: '20289',
+        active: true,
+        position: {
+            lat: 21.849831955539646,
+            lng: -102.29278399102525
+        }
+    },
+    {
+        id: 2,
+        name: 'ZAPATERIA PETITS',
+        state: 'AGUASCALIENTES',
+        city: 'AGUASCALIENTES',
+        neighborhood: 'PILAR BLANCO',
+        address: 'COMERCIAL VILLA ASUNCION',
+        numExt: '24',
+        numInt: 'A',
+        cp: '20280',
+        active: true,
+        position: {
+            lat: 21.850837006217784,
+            lng: -102.29311683879247
+        }
+    },
+    {
+        id: 3,
+        name: 'ZAPATERIA BINGO',
+        state: 'AGUASCALIENTES',
+        city: 'AGUASCALIENTES',
+        neighborhood: 'CENTRO',
+        address: 'MORELOS',
+        numExt: '228',
+        cp: '20000',
+        active: true,
+        position: {
+            lat: 21.882678927787943,
+            lng: -102.29502607913129
+        }
+
+    },
+    {
+        id: 4,
+        name: 'ZAPATERIA MIS PRIMOS',
+        state: 'AGUASCALIENTES',
+        city: 'PABELLON DE ARTEAGA',
+        neighborhood: 'CENTRO',
+        address: 'VALENTIN GOMEZ FARIAS',
+        numExt: '17',
+        numInt: 'A',
+        cp: '20670',
+        active: true,
+        position: {
+            lat: 22.14615497262452,
+            lng: -102.27579010262106
+        }
+    },
+    {
+        id: 5,
+        name: 'ZAPATERIA MICKEY',
+        state: 'BAJA CALIFORNIA NORTE',
+        city: 'ENSENADA',
+        neighborhood: 'CENTRO',
+        address: 'AVENIDA RUIZ',
+        numExt: '460',
+        cp: '22800',
+        active: true,
+        position: {
+            lat: 31.866780542650176,
+            lng: -116.62397166316356
+        }
+
+    },
+    {
+        id: 6,
+        name: 'EL MUNDO DE CHAVITO',
+        state: 'BAJA CALIFORNIA NORTE',
+        city: 'MEXICALI',
+        neighborhood: 'JABONERA',
+        address: 'LOPEZ MATEOS',
+        numExt: '29',
+        numInt: 'D',
+        cp: '21100',
+        active: true,
+        position: {
+            lat: 32.6576899142505,
+            lng: -115.4769305005778
+        }
+    }
+    , {
+        id: 7,
+        name: 'ZAPATERIAS ESTRELLA',
+        state: 'BAJA CALIFORNIA NORTE',
+        city: 'TIJUANA',
+        neighborhood: 'LIBERTAD',
+        address: 'PINO SUAREZ',
+        numExt: '1570',
+        cp: '22400',
+        active: true,
+        position: {
+            lat: 32.5349511112285,
+            lng: -117.00950892641488
+        }
+    }
+    , {
+        id: 8,
+        name: 'ZAPATERIA PIECITOS TIJUANA',
+        state: 'BAJA CALIFORNIA NORTE',
+        city: 'TIJUANA',
+        neighborhood: 'MADERO',
+        address: 'COLOMBIA',
+        numExt: '9112',
+        numInt: '',
+        cp: '22040',
+        active: true,
+        position: {
+            lat: 32.52051275031735,
+            lng: -117.0257667318929
+        }
+    },
+    {
+        id: 9,
+        name: 'ZAPATERIA MINI PAPOS',
+        state: 'BAJA CALIFORNIA NORTE',
+        city: 'TIJUANA',
+        neighborhood: 'AGUA CALIENTE',
+        address: 'BOULEVARD AGUA CALIENTE',
+        numExt: '11988',
+        numInt: '2',
+        cp: '20289',
+        active: true,
+        position: {
+            lat: 32.51012648088687,
+            lng: -116.99428282097283
+        }
+    },
+]
